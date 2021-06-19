1a8268a8a7d9afd75c668203c09d799a09f2b74e
TZZ <1789803837@qq.com>
2021-06-20 02:10:01 +0800

    完成颜色控制
    无法调整W值

--- LED.ino
+++ LED.ino
@@ -1,45 +1,6 @@
 /* *****************************************************************
  *
- * Download latest Blinker library here:
- * https://github.com/blinker-iot/blinker-library/archive/master.zip
- * 
- * 
- * Blinker is a cross-hardware, cross-platform solution for the IoT. 
- * It provides APP, device and server support, 
- * and uses public cloud services for data transmission and storage.
- * It can be used in smart home, data monitoring and other fields 
- * to help users build Internet of Things projects better and faster.
- * 
- * Make sure installed 2.7.4 or later ESP8266/Arduino package,
- * if use ESP8266 with Blinker.
- * https://github.com/esp8266/Arduino/releases
- * 
- * Make sure installed 1.0.5 or later ESP32/Arduino package,
- * if use ESP32 with Blinker.
- * https://github.com/espressif/arduino-esp32/releases
- * 
- * Docs: https://diandeng.tech/doc
- *       
- * 
- * *****************************************************************
- * 
- * Blinker 库下载地址:
- * https://github.com/blinker-iot/blinker-library/archive/master.zip
- * 
- * Blinker 是一套跨硬件、跨平台的物联网解决方案，提供APP端、设备端、
- * 服务器端支持，使用公有云服务进行数据传输存储。可用于智能家居、
- * 数据监测等领域，可以帮助用户更好更快地搭建物联网项目。
- * 
- * 如果使用 ESP8266 接入 Blinker,
- * 请确保安装了 2.7.4 或更新的 ESP8266/Arduino 支持包。
- * https://github.com/esp8266/Arduino/releases
- * 
- * 如果使用 ESP32 接入 Blinker,
- * 请确保安装了 1.0.5 或更新的 ESP32/Arduino 支持包。
- * https://github.com/espressif/arduino-esp32/releases
- * 
- * 文档: https://diandeng.tech/doc
- *       
+ * Smart Light Strip
  * 
  * *****************************************************************/
 
@@ -58,6 +19,10 @@
 #define PIN 8        // arduino control pin
 #define NUMPIXELS 30 // light numbers
 
+#define ON "on"
+#define OFF "off"
+#define COLORWHEEL "colorWheel"
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 BlinkerButton Button1("btn-switch");
@@ -65,27 +30,27 @@ BlinkerNumber Number1("num-abc");
 
 int counter = 0;
 
+int color[4] = {255, 255, 255, 255}; // RGBW
+
 void button1_callback(const String &state)
 {
     BLINKER_LOG("get button state: ", state);
 
-    String on = "on";
-    String off = "off";
-    if (state == "on")
+    if (state == ON)
     {
-        Button1.print("on");
+        Button1.print(ON);
 
         digitalWrite(LED_BUILTIN, LOW);
 
-        lightSwitch(on);
+        lightSwitch(ON);
     }
-    else if (state == "off")
+    else if (state == OFF)
     {
-        Button1.print("off");
+        Button1.print(OFF);
 
         digitalWrite(LED_BUILTIN, HIGH);
 
-        lightSwitch(off);
+        lightSwitch(OFF);
     }
 }
 
@@ -100,9 +65,10 @@ void dataRead(const String &data)
     BLINKER_LOG("key  : ", key);
     BLINKER_LOG("value: ", value);
 
-    // if (key.compare("btn-switch") == 0){
-
-    // }
+    if (key == COLORWHEEL)
+    {
+        lightSwitch(ON);
+    }
 
     counter++;
     Number1.print(counter);
@@ -150,6 +116,26 @@ void parseJson(const String &json, String &key, String &value)
     {
         key = p.key().c_str();
         value = p.value().as<char *>();
+
+        if (value == "")
+        {
+            JsonArray arr = p.value().as<JsonArray>();
+            BLINKER_LOG(arr);
+
+            if (key == COLORWHEEL)
+            {
+                int i = 0;
+                for (JsonVariant v : arr)
+                {
+                    color[i++] = v.as<int>();
+                }
+            }
+
+            // color[0] = arr[0];
+            // color[1] = arr[1];
+            // color[2] = arr[2];
+            // color[3] = arr[3];
+        }
         return;
     }
 }
@@ -157,16 +143,17 @@ void parseJson(const String &json, String &key, String &value)
 void lightSwitch(const String &state)
 {
     BLINKER_LOG("light stat:", state);
-    if (state == "on")
+    if (state == ON)
     {
+        BLINKER_LOG("COLOR:", color[0], " ", color[1], " ", color[2], " ", color[3]);
         for (int i = 0; i < NUMPIXELS; i++)
         {
-
-            pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+            // GRB
+            pixels.setPixelColor(i, pixels.Color(color[0], color[1], color[2], color[3]));
             pixels.show();
         }
     }
-    else if (state == "off")
+    else if (state == OFF)
     {
         pixels.clear();
 
